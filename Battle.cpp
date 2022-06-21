6ec38721fffa5b7c35140c43efd05e3024308843
mohamadkhalaily <103219002+mohamadkhalaily@users.noreply.github.com>
2022-06-21 23:11:03 +0300

    Update Battle.cpp

--- Battle.cpp
+++ Battle.cpp
@@ -1,17 +1,4 @@
-//
-// Created by momok on 6/14/2022.
-//
-
-#include "Battle.h"
-/*
-Battle::Battle() :card() ,force(0),loot(0),hp_on_loss(0),type(" ")
-{
-
-}
-*/
-Battle::Battle(int force,int loot ,int hp_on_loss, string& type):card("Battle"),force(force),loot(loot),
-hp_on_loss(hp_on_loss),type(type)
-{
-
-}
-
+////---Constructor---////
+Battle::Battle(int force,int loot ,int hp_on_loss, const string& type):Card(type),force(force),loot(loot),
+hp_on_loss(hp_on_loss)
+{}
